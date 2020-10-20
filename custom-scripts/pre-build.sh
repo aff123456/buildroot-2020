3f3f4cf81c7cb94372ec59dccedbd8ffa58b1d3e
aff123456 <gabri.serratine@gmail.com>
2020-10-20 20:37:20 -0300

    modificação pre-build.sh

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,4 +1,4 @@
   #Compile the syscall_test.c
   BUILDROOT_DIR=$BASE_DIR/..
-  COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+  COMPILER=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi-gcc
   $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
