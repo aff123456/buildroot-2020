cfacd19c6324ae553c15e35881d22f3cca3129c7
aff123456 <gabri.serratine@gmail.com>
2020-10-29 12:30:07 -0300

    formatação dos arquivos

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,5 +1,5 @@
 #Compile the syscall_test.c
-  BUILDROOT_DIR=$BASE_DIR/..
-  COMPILER=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi-gcc
-  $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
-  make ARCH=arm CROSS_COMPILE=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -C $BUILDROOT_DIR/modules/hello/
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
+make ARCH=arm CROSS_COMPILE=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -C $BUILDROOT_DIR/modules/hello/
