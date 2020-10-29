c15a8b8404f26459edd87e637541882bcff0130a
aff123456 <gabri.serratine@gmail.com>
2020-10-29 12:56:20 -0300

    modificações pre-build.sh para incluir a pasta simple_driver

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -3,3 +3,4 @@ BUILDROOT_DIR=$BASE_DIR/..
 COMPILER=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi-gcc
 $COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
 make ARCH=arm CROSS_COMPILE=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -C $BUILDROOT_DIR/modules/hello/
+make ARCH=arm CROSS_COMPILE=$BUILDROOT_DIR/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -C $BUILDROOT_DIR/modules/simple_driver
